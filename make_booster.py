40d97620e04ef9f1b40a966ffd3218acf4d99c7d
Your Name <gim.kobayashi@gmail.com>
2017-06-08 17:11:37 +0900

    a

--- make_booster.py
+++ make_booster.py
@@ -7,47 +7,49 @@ import random
 import glob
 import re
 
+import xgboost as xgb
 
 def to_svm(arr):
   return " ".join([ "%d:%09f"%(e,a) for e, a in enumerate(arr) ])
 
-for name in glob.glob("tag_pair/*.pkl"):
-  try:
-    pair = pickle.loads( open(name, "rb").read() )
-  except EOFError as e:
-    print(e)
-    continue
 
-  ps = list(map(lambda x:"1 " + to_svm(x), pair["positive"].values()))
+def train():
+  for name in glob.glob("tag_pair/*.pkl"):
+    try:
+      pair = pickle.loads( open(name, "rb").read() )
+    except EOFError as e:
+      print(e)
+      continue
 
-  ns = list(map(lambda x:"0 " + to_svm(x), pair["negative"].values()))
+    ps = list(map(lambda x:"1 " + to_svm(x), pair["positive"].values()))
 
-  ts = ps + ns
+    ns = list(map(lambda x:"0 " + to_svm(x), pair["negative"].values()))
 
-  random.shuffle(ts)
+    ts = ps + ns
 
-  tl = len(ts)
-  term = re.search(r"/(.*?)\.pkl", name).group(1)
-  dtrain  = "\n".join( ts[:int(tl*0.8)] )
-  dtest   = "\n".join( ts[int(tl*0.8):] )
-  open("booster_data/{}.train.txt".format(term), "w").write( dtrain )  
-  open("booster_data/{}.test.txt".format(term), "w").write( dtrain )  
-  print(term, len(ts) )
+    random.shuffle(ts)
 
-  CONF = """
-  objective = binary:logistic
-  eta = 0.025
-  gamma = 0
-  min_child_weight = 1 
-  max_depth = 30000
-  num_round = 300
-  save_period = 0 
-  data = "booster_data/{train}" 
-  eval[test] = "booster_data/{test}" 
-  test:data = "booster_data/{test}"      
-  """
+    tl = len(ts)
+    term = re.search(r"/(.*?)\.pkl", name).group(1)
+    dtrain  = "\n".join( ts[:int(tl*0.8)] )
+    dtest   = "\n".join( ts[int(tl*0.8):] )
+    open("booster_data/{}.train.txt".format(term), "w").write( dtrain )  
+    open("booster_data/{}.test.txt".format(term), "w").write( dtrain )  
+    print(term, len(ts) )
 
-  CONF = CONF.format(train="{}.train.txt".format(term), \
-              test="{}.test.txt".format(term), ) 
-  
-  open("booster_conf/{}.conf".format(term), "w").write( CONF )  
+    """ train & save models """
+    save_name = "booster_models/{}.model".format(term)
+    dtrain    = xgb.DMatrix( "booster_data/{}.train.txt".format(term) )
+    dtest     = xgb.DMatrix( "booster_data/{}.test.txt".format(term) )
+    param     = {'max_depth':1000, 'eta':0.025, 'silent':1, 'objective':'binary:logistic' }
+    num_round = 300
+    evallist  = [(dtest,'eval'), (dtrain,'train')]
+    bst       = xgb.train( param, dtrain, num_round, evallist )
+    bst.dump_model(save_name)
+
+    for ps in bst.predict(dtest):
+      print(ps)
+
+if __name__ == '__main__':
+  if '--train' in sys.argv:
+    train()
